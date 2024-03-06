297b54b924c3a4fea5797321d50bbba46d18b150
ChrisferDev <ferchris82@hotmail.com>
2024-03-05 22:04:12 -0500

    segundo commit

--- src/main/java/com/bolsadeideas/springboot/app/springbootweb/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/app/springbootweb/controllers/IndexController.java
@@ -1,43 +1,38 @@
 package com.bolsadeideas.springboot.app.springbootweb.controllers;
 
-import java.util.ArrayList;
+
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+
 
 import com.bolsadeideas.springboot.app.springbootweb.models.Usuario;
 
 @Controller
 @RequestMapping("/app")
 public class IndexController {
-    
-    //Métodos
+
+    @Value("${texto.indexcontroller.index.titulo}")
+    private String textoIndex;
+
+    @Value("${texto.indexcontroller.perfil.titulo}")
+    private String textoPerfil;
+
+    @Value("${texto.indexcontroller.listar.titulo}")
+    private String textoListar;
+
     @GetMapping({"/index", "/", "", "/home"})
     public String index(Model model){
-        model.addAttribute("titulo", "Hola Spring Framework con Model!");
+        model.addAttribute("titulo", textoIndex);
         return "index";
     }
-
-    /*@GetMapping({"/index", "/", "/home"})
-    public String index(Map<String, Object> map){
-        map.put("titulo", "Hola Spring Framework con Map!");
-        return "index";
-    }*/
-
-    /*@GetMapping({"/index", "/", "/home"})
-    public ModelAndView index(ModelAndView mv){
-        mv.addObject("titulo", "Hola Spring Framework con ModelAndView!");
-        mv.setViewName("index");
-        return mv;
-    }*/
 
     @RequestMapping("/perfil")
     public String perfil(Model model){
@@ -48,15 +43,14 @@ public class IndexController {
         usuario.setEmail("chris@mail.com");
 
         model.addAttribute("usuario", usuario);
-        model.addAttribute("titulo", "Perfil de Usuario: ".concat(usuario.getNombre()));
+        model.addAttribute("titulo", textoPerfil.concat(usuario.getNombre()));
 
         return "perfil";
     }
 
     @RequestMapping("/listar")
     public String listar(Model model){
-        
-        model.addAttribute("titulo", "Listado de usuarios");
+        model.addAttribute("titulo", textoListar);
 
         return "listar";
     }
